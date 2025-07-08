7f741f13b05ab3f79ffb048be53866ad624d3288
Elias-Doc <eliasmunguambe617@gmail.com>
2025-07-08 08:16:22 +0200

    Update app.py

--- app.py
+++ app.py
@@ -55,4 +55,6 @@ def recibo(id):
     return render_template('recibo_final.html', consulta=consulta)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+    
